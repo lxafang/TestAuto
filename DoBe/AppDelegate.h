8757652a22861d4e618adedd62c692dc34c2b7c1
liuxuan <liuxuan@yongche.com>
2015-06-01 15:54:24 +0800

    Initial Commit

---
+++ DoBe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoBe
+//
+//  Created by liuxuan on 15/6/1.
+//  Copyright (c) 2015年 liuxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DoBe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DoBe
+//
+//  Created by liuxuan on 15/6/1.
+//  Copyright (c) 2015年 liuxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
